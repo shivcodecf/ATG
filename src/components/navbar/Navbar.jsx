613825219a82c2ba0a03062e3b9f40ecbff1e9cd
shivcodecf <shivam2113128@akgec.ac.in>
2024-04-03 18:22:40 +0530

    test commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,7 +7,9 @@ import exp from '../../assets/exp.png'
 import hob from '../../assets/hob.png' 
 import sav from '../../assets/sav.png' 
 import not from '../../assets/not.png' 
-import cart from '../../assets/cart.png' 
+import cart from '../../assets/cart.png'  
+import { AiOutlineMenu } from "react-icons/ai";
+
 
 
 const Navbar = () => {
@@ -33,7 +35,7 @@ const Navbar = () => {
   <button class="btn btn-secondary dropdown-toggle drop-btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
     Explore
   </button>
-  <ul class="dropdown-menu">
+  <ul class="dropdown-menu ">
     <li><a class="dropdown-item" href="#">People-Community</a></li>
     <li><a class="dropdown-item" href="#">Places-venues</a></li>
     <li><a class="dropdown-item" href="#">Programs-Events</a></li> 
@@ -42,13 +44,13 @@ const Navbar = () => {
   </ul>
 </div>
         </div>  
-        <div className="exp"> 
+        <div className="exp "> 
         <img src={hob} alt="" />  
         <div class="dropdown">
   <button class="btn btn-secondary dropdown-toggle drop-btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
     hobbies
   </button>
-  <ul class="dropdown-menu">
+  <ul class="dropdown-menu" >
     <li><a class="dropdown-item" href="#">play games</a></li>
     <li><a class="dropdown-item" href="#">Competative Programming</a></li>
     <li><a class="dropdown-item" href="#">Singing Song</a></li>
@@ -56,12 +58,15 @@ const Navbar = () => {
 </div>
         </div> 
 
-         <img src={sav} alt="" />
-         <img src={not} alt="" />
-         <img src={cart} alt="" /> 
-        <div className="sign-btn">
+         <img src={sav} alt="" className='no-visible' /> 
+         <CiSearch className='visible' style={{marginTop:"30px",marginLeft:"700px"}}/>
+         <img src={not} alt=""  className='visible' style={{marginLeft:"10px",fontSize:"larger"}} / >
+         <img src={cart} alt=""  className='no-visible' /> 
+        <div className="sign-btn  no-visible" >
         <button>Sign In</button> 
-        </div>
+        </div>    
+        <AiOutlineMenu className='visible' style={{marginTop:"30px",marginLeft:"5px"}}/>
+
 
      </div> 
 
